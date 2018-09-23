1d7bc5f6e639fbc2b0cc706fb7d78929b4ef165d
yuryweiland <yurem@bk.ru>
2018-09-23 13:31:43 +0300

    (fix): eslist fixes

--- js/stats.js
+++ js/stats.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-var CLOUD_WIDTH = 370; // Ширина облака, пкс
+var CLOUD_WIDTH = 270; // Ширина облака, пкс
 var CLOUD_HEIGHT = 270; // Высота облака, пкс
 var CLOUD_X = 150; // Отступ слева от облака, пкс
 var CLOUD_Y = 0; // Отступ сверху от облака, пкс
@@ -28,115 +28,115 @@ var STATS_TEXT_OFFSET = 70;
 
 // Рисуем облака
 function renderCloud(ctx, x, y, color) {
-    drawCloud(ctx, x, y, color);
-    drawCloudShadow(ctx, x, y, color);
+  drawCloud(ctx, x, y, color);
+  drawCloudShadow(ctx, x, y, color);
 }
 
 // Рисуем основное облако результата
 function drawCloud(ctx, x, y, color) {
-    ctx.fillStyle = color; // Цвет заливки
-    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT); // Заливаем облако цветом
+  ctx.fillStyle = color; // Цвет заливки
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT); // Заливаем облако цветом
 }
 
 // Рисуем тень
 function drawCloudShadow(ctx, x, y, color) {
-    ctx.fillStyle = color;
-    ctx.strokeRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT); // Контур вокруг облака/тени
+  ctx.fillStyle = color;
+  ctx.strokeRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT); // Контур вокруг облака/тени
 }
 
 // Получечние наибольшего результата в статистике
 function getMaxResult(arr) {
-    // Заглушка - если в arr - пустой массив - добавляем в него 4 рандомных результата
-    if (arr.length === 0) {
-        for (var j = 0; j < 4; j++) {
-            arr.push(Math.floor(Math.random() * MAX_VALUE_STATS));
-        }
+  // Заглушка - если в arr - пустой массив - добавляем в него 4 рандомных результата
+  if (arr.length === 0) {
+    for (var j = 0; j < 4; j++) {
+      arr.push(Math.floor(Math.random() * MAX_VALUE_STATS));
     }
+  }
 
-    var maxResult = arr[0];
+  var maxResult = arr[0];
 
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] > maxResult) {
-            maxResult = arr[i];
-        }
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxResult) {
+      maxResult = arr[i];
     }
+  }
 
-    return maxResult;
+  return maxResult;
 }
 
 // Сравниваем длину массивов с именами игроков и кол-вом попыток
 // Если длины массивов не равны - сравниваем по наибольшему
 function equalArrays(names, times) {
-    if (names.length !== times.length) {
-        if (names.length > times.length) {
-            times.length = names.length;
-        } else {
-            names.length = times.length;
-        }
+  if (names.length !== times.length) {
+    if (names.length > times.length) {
+      times.length = names.length;
+    } else {
+      names.length = times.length;
     }
+  }
 }
 
 // Рендерим текст
 function renderText(ctx) {
-    ctx.fillStyle = FIGURE_COLOR; // Заливка шрифта
-    ctx.font = FONT_STYLE; // Заливка шрифта
-    ctx.textBaseline = FONT_BASELINE; // Выровнить шрифт по базовой линии
-    ctx.fillText('Ура вы победили!', CLOUD_X + TEXT_OFFSET, CLOUD_Y + TEXT_OFFSET); // Текст
-    ctx.fillText('Список результатов:', CLOUD_X + TEXT_OFFSET, CLOUD_Y + TEXT_TOP_OFFSET); // Текст
+  ctx.fillStyle = FIGURE_COLOR; // Заливка шрифта
+  ctx.font = FONT_STYLE; // Заливка шрифта
+  ctx.textBaseline = FONT_BASELINE; // Выровнить шрифт по базовой линии
+  ctx.fillText('Ура вы победили!', CLOUD_X + TEXT_OFFSET, CLOUD_Y + TEXT_OFFSET); // Текст
+  ctx.fillText('Список результатов:', CLOUD_X + TEXT_OFFSET, CLOUD_Y + TEXT_TOP_OFFSET); // Текст
 }
 
 // Рендерим заголовок
 function renderStatsName(ctx, names, times, i, maxTime) {
-    // Текст
-    var textXHeader = CLOUD_X + TEXT_OFFSET_STATS + (BAR_WIDTH + TEXT_WIDTH) * i;
-    var parsedTime = parseInt(times[i], 10);
+  // Текст
+  var textXHeader = CLOUD_X + TEXT_OFFSET_STATS + (BAR_WIDTH + TEXT_WIDTH) * i;
+  var parsedTime = parseInt(times[i], 10);
 
-    ctx.fillStyle = TEXT_HEAD_COLOR;
-    ctx.fillText(parsedTime, textXHeader, (CLOUD_Y + CLOUD_HEIGHT - STATS_TEXT_OFFSET) - (BAR_HEIGHT * times[i]) / maxTime);
-    ctx.fillText(names[i], textXHeader, CLOUD_Y + CLOUD_HEIGHT - TEXT_BOTTOM_OFFSET);
+  ctx.fillStyle = TEXT_HEAD_COLOR;
+  ctx.fillText(parsedTime, textXHeader, (CLOUD_Y + CLOUD_HEIGHT - STATS_TEXT_OFFSET) - (BAR_HEIGHT * times[i]) / maxTime);
+  ctx.fillText(names[i], textXHeader, CLOUD_Y + CLOUD_HEIGHT - TEXT_BOTTOM_OFFSET);
 }
 
 function renderStats(ctx, names, times) {
-    var maxTime = getMaxResult(times); // Поиск максимального результата
+  var maxTime = getMaxResult(times); // Поиск максимального результата
 
-    // Бежим по циклу результатов (ограничиваем кол-вом имён участников)
-    for (var i = 0; i < names.length; i++) {
+  // Бежим по циклу результатов (ограничиваем кол-вом имён участников)
+  for (var i = 0; i < names.length; i++) {
 
-        // Координата верхней левой точки прямоугольника (по X)
-        var statsX = CLOUD_X + TEXT_OFFSET_STATS + (BAR_WIDTH + BAR_OFFSET) * i;
+    // Координата верхней левой точки прямоугольника (по X)
+    var statsX = CLOUD_X + TEXT_OFFSET_STATS + (BAR_WIDTH + BAR_OFFSET) * i;
 
-        // Координата верхней левой точки прямоугольника (по Y)
-        var statsY = CLOUD_Y + CLOUD_HEIGHT - BAR_OFFSET;
+    // Координата верхней левой точки прямоугольника (по Y)
+    var statsY = CLOUD_Y + CLOUD_HEIGHT - BAR_OFFSET;
 
-        // Ширина прямоугольника
-        var barResultStats = -(BAR_HEIGHT * times[i]) / maxTime;
+    // Ширина прямоугольника
+    var barResultStats = -(BAR_HEIGHT * times[i]) / maxTime;
 
-        // Высота прямоугольника
-        renderStatsName(ctx, names, times, i, maxTime);
+    // Высота прямоугольника
+    renderStatsName(ctx, names, times, i, maxTime);
 
-        if (names[i] === 'Вы') {
-            ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-        } else {
-            ctx.fillStyle = 'rgba(0, 0, ' + Math.floor(Math.random() * 255) + ', 1)';
-        }
-
-        ctx.fillRect(statsX, statsY, BAR_WIDTH, barResultStats);
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, ' + Math.floor(Math.random() * 255) + ', 1)';
     }
+
+    ctx.fillRect(statsX, statsY, BAR_WIDTH, barResultStats);
+  }
 }
 
 window.renderStatistics = function (ctx, names, times) {
-    // Приводим кол-во результатов к кол-ву игроков
-    equalArrays(names, times);
+  // Приводим кол-во результатов к кол-ву игроков
+  equalArrays(names, times);
 
-    // Рендерим облако
-    renderCloud(ctx, CLOUD_X + OFFSET, CLOUD_Y + OFFSET, SHADOW_COLOR);
+  // Рендерим облако
+  renderCloud(ctx, CLOUD_X + OFFSET, CLOUD_Y + OFFSET, SHADOW_COLOR);
 
-    // Рендерим подложку (тень) под облаком
-    renderCloud(ctx, CLOUD_X, CLOUD_Y, BACK_COLOR);
+  // Рендерим подложку (тень) под облаком
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, BACK_COLOR);
 
-    // Рендер текста
-    renderText(ctx);
+  // Рендер текста
+  renderText(ctx);
 
-    // Рендер результатов
-    renderStats(ctx, names, times);
+  // Рендер результатов
+  renderStats(ctx, names, times);
 };
